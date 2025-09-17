32c5b8a99a44003f34283433686bf83450fef88b
MohamedWaseem07 <mohamedwasim96@gmail.com>
2025-09-17 10:19:07 +0530

    Added Calculator
    First Project

---
+++ 001_variables.py
@@ -0,0 +1,17 @@
+x = 100
+y = 10.8
+Name = "Wasim"
+print(x)
+print(type(x))
+
+print(y)
+print(type(y))
+
+print(Name)
+print(type(Name))
+
+x= 110
+y= 90
+z = x+y
+print(z)
+
---
+++ 002_first.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+c = a + b
+print('Sum is' , c)
+print('Hello Wasim')
+
+
+length = 15
+breadth = 5
+area = length * breadth
+print('Area is', area)
+
+
+prices = [10,7,5,8,6]
+quantity = [1,2,3,1,1]
